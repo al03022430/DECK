d2d787c5224fb7bed5516e9a8986d2cdfe8c7b37
Roland <al03022430@tecmilenio.mx>
2023-06-07 15:57:08 -0600

    1er Avance Actividad 3

---
+++ src/DECK/DECK.java
@@ -0,0 +1,22 @@
+package DECK;
+import java.awt.*;
+import java.until.*;
+import java.util.ArrayList;
+
+public class Deck {
+    Public static void (String [] args){
+        CARD card = new CardLayout();
+        int PALOS = CARD.Palo.lenght;
+        int VALORES = CARD.Valor.length;
+        int COLORES = CARD.Color.length;
+        int C = PALOS * VALORES * COLORES;
+
+        ArrayList<string> DECK = new ArrayList<string>();
+        for (int w = 0; < VALORES; w++){
+            for (int e=0; j < PALOS; e++){
+                Deck.add(card.Palo[e]+ ", ROJO , " + CARD.VALOR [w] )
+            }
+
+        }
+    }
+}
